650215a861eaad045694527324735157e810c103
profamaroca <profamaroca@gmail.com>
2021-11-24 21:19:36 -0300

    acrescentando EJS template engine

--- app.js
+++ app.js
@@ -10,6 +10,35 @@ app.get('/', (req,res) => {
     res.send('<h1> rota em construção! </h1>')
 })
 
+// configurar qual a view engine
+app.set('view engine', 'ejs');
+// configurar o diretório onde estão as nossas views/páginas/visualizações/frontend
+app.set('views', './views');
+
+
+// simulando um array de dados que pode ter vindo de um banco de dados mongoDB
+var pessoasArray = [
+    {
+        'nome': 'Pedro',
+        'idade': 12
+    },
+    {
+        'nome': 'Mariana',
+        'idade': 33,
+    },
+    {
+        'nome': 'Bia',
+        'idade': 25,
+    },
+]
+
+
+// configurar uma rota para requisição GET na raiz /
+app.get('/pessoas', (req,res) => {
+    // renderiza a página/view pessoas, informando os dados que serão exibidos
+    res.render('pessoas', {dados: pessoasArray});
+})
+
 // configurar uma rota para requisição GET no caminho /livros
 // devolve uma resposta simples contendo um título
 app.get('/livros', (req, res) => {
